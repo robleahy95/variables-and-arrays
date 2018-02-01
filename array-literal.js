8a7e145254df5ea489d9e7c88c22895dabce6e9f
robleahy95 <35769579+robleahy95@users.noreply.github.com>
2018-02-01 16:16:46 +0000

    Add files via upload
    array added

---
+++ array-literal.js
@@ -0,0 +1,7 @@
+//create an array and assign it values
+var colors;
+colors = ['white', 'black', 'red'];
+
+//show the first item from the array.
+var el = document.getElementById('colors');
+el.textContent = colors[0];
